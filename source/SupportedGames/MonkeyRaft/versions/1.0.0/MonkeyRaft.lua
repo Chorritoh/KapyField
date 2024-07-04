bb8b603441b699af1d533ecae39b3a129576f7e6
Papubaras <111273482+Chorritoh@users.noreply.github.com>
2024-07-04 16:41:24 -0700

    Create MonkeyRaft.lua

---
+++ source/SupportedGames/MonkeyRaft/versions/1.0.0/MonkeyRaft.lua
@@ -0,0 +1,12 @@
+--"Monkey Raft Best Roblox Monkey Game fr fr. 🗣🔥🔥"
+--Script made by roubloxsaa. :)
+
+local KapyField = loadstring(game:HttpGet('https://raw.githubusercontent.com/Chorritoh/KapyHub/main/source/KapyField/versions/0.1.1/KapyField.lua'))()
+
+--=====================================================================
+--                              Variables
+--=====================================================================
+
+
+--=====================================================================
+--=====================================================================
